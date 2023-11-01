e408e325fee40f18ae9c2db5a6e8d55a0d7c1828
mariacabriles29 <mecp2992@gmail.com>
2023-11-01 15:00:39 -0400

    feat: creating navigation bar

--- demo-initial/src/app/about/page.tsx
+++ demo-initial/src/app/about/page.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import styles from "./page.module.css";
+import styles from "../page.module.css";
 import Link from "next/link";
+import { Navbar } from "@/components/Navbar";
 
 export default function About() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>
-        Ir a <Link href="/">Home</Link>
-      </h1>
+    <>
+      <Navbar />
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Ir a <Link href="/">Home</Link>
+        </h1>
 
-      <div className={styles.description}>
-        <p>Get started by editing&nbsp;</p>
-      </div>
-    </main>
+        <div className={styles.description}>
+          <p>Get started by editing&nbsp;</p>
+        </div>
+      </main>
+    </>
   );
 }
--- demo-initial/src/app/page.tsx
+++ demo-initial/src/app/page.tsx
@@ -1,19 +1,24 @@
 import Image from "next/image";
+
 import styles from "./page.module.css";
 import Link from "next/link";
+import { Navbar } from "@/components/Navbar";
 
 export default function HomePage() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>
-        Ir a <Link href="/about">About</Link>
-      </h1>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-      </div>
-    </main>
+    <>
+      <Navbar />
+      <main className={styles.main}>
+        <h1>
+          Ir a <Link href="/about">About</Link>
+        </h1>
+        <div className={styles.description}>
+          <p>
+            Get started by editing&nbsp;
+            {/* <code className={styles.code}>src/app/page.tsx</code> */}
+          </p>
+        </div>
+      </main>
+    </>
   );
 }
